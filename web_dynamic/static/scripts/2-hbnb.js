a68b521950a5a56a32ca0732d164290661c3c186
Alex Rivera <alexriveracruz4@gmail.com>
2021-09-24 16:14:49 +0000

    [UPDATE] url

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,8 +1,8 @@
 // Listen for changes on each input checkbox tag.
 $( document ).ready(function () {
-  const url = 'http://0.0.0.0:5001/api/v1/status/';
-  $.get(url, function (response) {
-    if (response.status === 'OK') {
+  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
+  $.get(url, function (data) {
+    if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
